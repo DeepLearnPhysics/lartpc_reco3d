Transformers fix in normalization and activation layer

analysis/producers/scripts/pid_metrics.py
@@ -3,6 +3,8 @@ import numpy as np
 from mlreco.utils.globals import *
 
 from mlreco.utils.gnn.cluster import get_cluster_label
+from mlreco.utils.tracking import get_track_segment_dedxs, get_track_deposition_gradient
+from mlreco.utils.gnn.cluster import get_cluster_dedxs
 
 @write_to(['pid_metrics'])
 def pid_metrics(data_blob, res, **kwargs):
@@ -13,22 +15,31 @@ def pid_metrics(data_blob, res, **kwargs):
     metrics = []
     image_idxs = data_blob['index']
     meta       = data_blob['meta'][0]
-    
-    
 
     for idx, index in enumerate(image_idxs):
         
-        logits = res['logits'][idx]
+        if 'node_pred' in res:
+            logits = res['node_pred'][idx]
+        elif 'logits' in res:
+            logits = res['logits'][idx]
         clusts = res['clusts'][idx]
+        
         particles = { p.id(): p for p in data_blob['particles_asis'][idx] }
         
         data = data_blob['input_data'][idx]
+        
+        # print(data[])
         
         labels = get_cluster_label(data, clusts, column=PID_COL)
         nu_labels = get_cluster_label(data, clusts, column=NU_COL)
         pgrps = get_cluster_label(data, clusts, column=PGRP_COL)
         pshows = get_cluster_label(data, clusts, column=PSHOW_COL)
         group_labels = get_cluster_label(data, clusts, column=GROUP_COL)
+        shape_labels = get_cluster_label(data, clusts, column=SHAPE_COL)
+        
+        startpoints = data_blob['particles_label'][idx][:, COORD_COLS]
+        
+        shower_dedx = get_cluster_dedxs(data[:, COORD_COLS], data[:, VALUE_COL], startpoints, clusts, max_dist=10)
 
         index_dict = {
             'Iteration': kwargs['iteration'],
@@ -41,6 +52,7 @@ def pid_metrics(data_blob, res, **kwargs):
             p = particles.get(group_id, None)
             if p is None:
                 continue
+            
             pred = np.argmax(score)
             truth = int(labels[j])
             out_dict = index_dict.copy()
@@ -52,7 +64,87 @@ def pid_metrics(data_blob, res, **kwargs):
             out_dict['energy_init'] = p.energy_init()
             out_dict['energy_deposit'] = p.energy_deposit()
             out_dict['creation_process'] = p.creation_process()
+            out_dict['shower_dedx'] = shower_dedx[j]
+            out_dict['semantic_type'] = int(shape_labels[j])
             for k, s in enumerate(score):
                 out_dict['Score_{}'.format(k)] = s
             metrics.append(out_dict)
     return [metrics]
+
+
+@write_to(['singlep_metrics'])
+def singlep_metrics(data_blob, res, **kwargs):
+    """
+    Select particle pairs for logging (only for mpv/nu interactions)
+    """
+
+    metrics = []
+    image_idxs = data_blob['index']
+    meta = data_blob['meta'][0]
+    
+    particles = data_blob['particles_asis'][0]
+    
+    particle_info = {
+        'creation_process': None,
+        'energy_init': None,
+        'energy_deposit': None
+    }
+    
+    out = {
+        'Index': image_idxs[0],
+        'Truth': int(data_blob['label'][0][:, 1]),
+        'Prediction': np.argmax(res['logits'][0])
+    }
+    
+    points = data_blob['input_data'][0][:, COORD_COLS]
+    values = data_blob['input_data'][0][:, VALUE_COL]
+    semantic_type = data_blob['input_data'][0][:, SHAPE_COL]
+    
+    
+    for p in particles:
+        if p.id() == p.parent_id():
+            particle_info['creation_process'] = p.creation_process()
+            particle_info['energy_init'] = p.energy_init()
+            particle_info['energy_deposit'] = p.energy_deposit()
+            particle_info['ke'] = p.energy_init() - PID_MASSES[out['Truth']]
+            
+            dist, dedx = compute_min_dist_to_startpoint(points, values, p, meta, r=5.0)
+            particle_info['min_dist'] = dist
+            particle_info['dedx'] = dedx
+            particle_info['pdg_code'] = p.pdg_code()
+            
+        
+    out.update(particle_info)
+    
+    metrics.append(out)
+    return [metrics]
+
+
+# -----------------------------------------------------------------------------
+# Helper functions
+# -----------------------------------------------------------------------------
+
+def pixel_to_cm_1d(vec, meta):
+    out = np.zeros_like(vec)
+    out[0] = meta[0] + meta[6] * vec[0]
+    out[1] = meta[1] + meta[7] * vec[1]
+    out[2] = meta[2] + meta[8] * vec[2]
+    return out
+
+def compute_min_dist_to_startpoint(points, values, particle_asis, meta, r=5.0):
+    
+    startpoint = np.array([getattr(particle_asis.first_step(), x)() for x in ['x', 'y', 'z']] )
+    
+    startpoint = pixel_to_cm_1d(startpoint, meta)
+    
+    dists = np.linalg.norm(points - startpoint, axis=1)
+    min_dist = dists.min()
+    min_index = np.argmin(dists)
+    
+    closest_point = points[min_index]
+    new_dists = np.linalg.norm(points - closest_point, axis=1)
+    
+    dedx = (values[new_dists < r].sum() + 1e-6) / (new_dists[new_dists < r].max() + 1e-6)
+    
+    return min_dist, dedx
+    

mlreco/models/experimental/layers/pointnet.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from torch_geometric.nn import MLP, PointNetConv, fps, global_max_pool, radius
+from mlreco.utils.globals import *
+from mlreco.utils.gnn.cluster import form_clusters, get_cluster_label
+from mlreco.utils.gnn.data import split_clusts
 
 # From Pytorch Geometric Examples for PointNet:
 # https://github.com/pyg-team/pytorch_geometric/blob/master/examples/pointnet2_classification.py
@@ -85,8 +88,8 @@ class PointNet(torch.nn.Module):
         self.sa3_module = GlobalSAModule(MLP(self.mlp_specs_glob))
         self.mlp = MLP(self.mlp_specs_final, dropout=self.dropout, norm=None)
 
-    def forward(self, data):
-        sa0_out = (data.x, data.pos, data.batch)
+    def forward(self, x, pos, batch):
+        sa0_out = (x, pos, batch)
 
         out = sa0_out
 
@@ -106,6 +109,59 @@ class PointNetEncoder(torch.nn.Module):
         self.net = PointNet(cfg)
         self.latent_size = self.net.latent_size
 
-    def forward(self, batch):
-        out = self.net(batch)
+    def forward(self, input_tensor, startpoints=None):
+        pos = input_tensor[:, COORD_COLS]
+        batch = input_tensor[:, BATCH_COL].long()
+        x = input_tensor[:, VALUE_COL].view(-1, 1)
+        
+        out = self.net(x, pos, batch)
         return out
+    
+    
+class PointNetMultiParticleEncoder(nn.Module):
+    
+    def __init__(self, cfg, name='pointnet_multi_encoder'):
+        super(PointNetMultiParticleEncoder, self).__init__()
+        self.encoder = PointNetEncoder(cfg)
+        self.latent_size = self.encoder.latent_size
+        self.split_col = GROUP_COL
+        self.batch_col = BATCH_COL
+        self.target_col = PID_COL
+        self.invalid_id = -1
+        
+    def split_input(self, point_cloud, clusts=None):
+        point_cloud_cpu  = point_cloud.detach().cpu().numpy()
+        batches, bcounts = np.unique(point_cloud_cpu[:,self.batch_col], return_counts=True)
+        if clusts is None:
+            clusts = form_clusters(point_cloud_cpu, column=self.split_col)
+        if not len(clusts):
+            return point_cloud, [np.array([]) for _ in batches], []
+
+        if self.skip_invalid:
+            target_ids = get_cluster_label(point_cloud_cpu, clusts, column=self.target_col)
+            clusts = [c for i, c in enumerate(clusts) if target_ids[i] != self.invalid_id]
+            if not len(clusts):
+                return point_cloud, [np.array([]) for _ in batches], []
+
+        split_point_cloud = point_cloud.clone()
+        split_point_cloud[:, self.batch_col] = -1
+        for i, c in enumerate(clusts):
+            split_point_cloud[c, self.batch_col] = i
+        
+        batch_ids = get_cluster_label(point_cloud_cpu, clusts, column=self.batch_col)
+        clusts_split, cbids = split_clusts(clusts, batch_ids, batches, bcounts)
+
+        return split_point_cloud[split_point_cloud[:,self.batch_col] > -1], clusts_split, cbids
+        
+    def forward(self, point_cloud, clusts):
+        
+        res = {}
+        
+        out, clusts_split, cbids = self.split_input(point_cloud, clusts)
+        res['clusts'] = [clusts_split]
+
+        out = self.encoder(out)
+        out = self.final_layer(out)
+        res['logits'] = [[out[b] for b in cbids]]
+        
+        return res

mlreco/models/experimental/losses/classification.py
@@ -4,6 +4,35 @@ import torch.nn.functional as F
 
 from mlreco.models.layers.cluster_cnn.losses.lovasz import lovasz_softmax_flat
 
+
+def classification_loss_dict(name):
+    """Loss function dictionary for classification tasks.
+
+    All loss functions assume that the inputs are of the form
+    (logits, labels), where logits is a tensor of shape (N, C) and
+    labels is a tensor of shape (N,) containing the class labels.
+    
+    Also assumes that reduction is set to 'mean' in the loss function.
+    """
+    
+    loss_dict = {
+        'cross_entropy': CrossEntropyLoss,
+        'focal': FocalLoss,
+        'dice': DiceLoss,
+        'lovasz_softmax': LovaszSoftmaxLoss,
+        'jaccard': JaccardLoss,
+        'tversky': TverskyLoss,
+        'focal_tversky': FocalTverskyLoss,
+        'log_cosh_dice': LogCoshDiceLoss,
+        'log_dice': LogDiceLoss
+    }
+    
+    constructor = loss_dict[name]
+    
+    print(f"Initialized {constructor.__name__} Loss for Classification.")
+    
+    return constructor
+
 class ClassificationLoss(nn.Module):
     """
     Abstract base class for all classification loss functions.
@@ -72,7 +101,7 @@ class CrossEntropyLoss(ClassificationLoss):
     
     def __init__(self, **kwargs):
         super(CrossEntropyLoss, self).__init__(**kwargs)
-        self.w = 1.0
+        self.w = 0.0
         
     def regularization(self, logits, labels):
         """

mlreco/models/experimental/transformers/attention.py
@@ -20,7 +20,8 @@ def _get_norm_layer(name):
 def _get_act_layer(name):
     act_dict = {
         'relu': nn.ReLU,
-        'leaky_relu': nn.LeakyReLU
+        'leaky_relu': nn.LeakyReLU,
+        'gelu': nn.GeLU
     }
     return act_dict[name]
 

mlreco/models/experimental/transformers/layers.py
@@ -144,7 +144,8 @@ class MultiHeadAttention(nn.Module):
 class PositionWiseFFN(nn.Module):
     
     def __init__(self, num_input, num_output, num_hidden, 
-                 leakiness=0.0, dropout=True, norm_layer='layer_norm'):
+                 dropout=True, 
+                 norm_layer='layer_norm', act_layer='relu', act_kwargs=None):
         super(PositionWiseFFN, self).__init__()
         self.num_input = num_input
         self.num_output = num_output
@@ -157,7 +158,10 @@ class PositionWiseFFN(nn.Module):
             self.norm = nn.BatchNorm1d(num_output)
         else:
             raise ValueError('Normalization layer {} not recognized!'.format(norm_layer))
-        self.act = nn.LeakyReLU(negative_slope=leakiness)
+        
+        if act_kwargs is None:
+            act_kwargs = {}
+        self.act = _get_activation_fn(act_layer, **act_kwargs)
 
         self.dropout = dropout
         if self.dropout:
@@ -342,12 +346,14 @@ class FFNLayer(nn.Module):
         return self.forward_post(tgt)
 
 
-def _get_activation_fn(activation):
+def _get_activation_fn(activation, **kwargs):
     """Return an activation function given a string"""
     if activation == "relu":
         return F.relu
     if activation == "gelu":
         return F.gelu
+    if activation == 'lrelu':
+        return partial(F.leaky_relu, negative_slope=kwargs['negative_slope'])
     if activation == "glu":
         return F.glu
     raise RuntimeError(F"activation should be relu/gelu, not {activation}.")

mlreco/models/experimental/transformers/models.py
@@ -178,10 +178,12 @@ class EdgeTransformer(nn.Module):
                         self.num_heads,
                         self.num_hidden,
                         edge_num_input=edge_num_input,
-                        leakiness=self.leakiness,
+                        # leakiness=self.leakiness,
                         dropout=self.dropout,
                         norm_layer=self.norm_layer,
-                        edge_include_mode=self.edge_include_mode))
+                        edge_include_mode=self.edge_include_mode,
+                        act_layer=self.act_layer,
+                        act_kwargs=self.act_kwargs))
             
         self.node_predictor = nn.Linear(self.num_output, self.node_classes)
         self.edge_predictor = nn.Linear(self.num_output, self.edge_classes)
@@ -195,7 +197,8 @@ class EdgeTransformer(nn.Module):
                              num_hidden=128,
                              dropout=0.0, 
                              leakiness=0.0,
-                             activation='relu', 
+                             act_layer='relu', 
+                             act_kwargs=None,
                              norm_layer='batch_norm',
                              use_bias=False,
                              edge_include_mode='prod',
@@ -209,7 +212,9 @@ class EdgeTransformer(nn.Module):
         self.num_hidden = num_hidden
         self.leakiness = leakiness
         self.dropout = dropout
-        self.activation = activation
+        self.act_layer = act_layer
+        if act_kwargs is None:
+            self.act_kwargs = {}
         self.norm_layer = norm_layer
         self.use_bias = use_bias
         self.edge_include_mode = edge_include_mode
@@ -293,9 +298,10 @@ class GraphTransformerEncoderLayer(nn.Module):
     
     def __init__(self, num_input, num_output, num_heads,
                  num_hidden, edge_num_input,
-                 leakiness=0.0,
                  dropout=0.0,
                  norm_layer='batch_norm',
+                 act_layer='relu',
+                 act_kwargs=None,
                  residual=True,
                  use_bias=False,
                  concat_attn=True,
@@ -329,15 +335,17 @@ class GraphTransformerEncoderLayer(nn.Module):
         
         self.ffn_node = PositionWiseFFN(num_output, num_output,
                                         num_hidden, 
-                                        leakiness=leakiness,
                                         dropout=dropout,
-                                        norm_layer=norm_layer)
+                                        norm_layer=norm_layer,
+                                        act_layer=act_layer,
+                                        act_kwargs=act_kwargs)
         
         self.ffn_edge = PositionWiseFFN(num_output, num_output,
                                         num_hidden, 
-                                        leakiness=leakiness,
                                         dropout=dropout,
-                                        norm_layer=norm_layer)
+                                        norm_layer=norm_layer,
+                                        act_layer=act_layer,
+                                        act_kwargs=act_kwargs)
         
         
     def forward(self, x, edge_index, edge_attr, xbatch=None):